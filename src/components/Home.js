fc72209286f0a4049cae8f0df2b2c770729d4042
Darshit Sorathiya <darshitgsorathiya7@gmail.com>
2022-02-10 01:49:54 +0530

    till creating Newsitem

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
-import {React,useContext} from 'react'
-import noteContext from '../context/notes/noteContext';
+import React from 'react'
+import Notes from './Notes'
+
 
 const Home = () => {
-  const context=useContext(noteContext);
-  const {notes,setNotes}=context;
+  
   return (
     <>
       <div className="container my-3">
@@ -27,12 +27,7 @@ const Home = () => {
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
-      <div className="container my-3">
-        <h2>Your Notes</h2>
-        {notes.map((note)=>{
-          return note.title;
-        })}
-      </div>
+      <Notes/>
 
     </>
   )
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -12,7 +12,50 @@ const initialnotes=[
       "tag": "personal",
       "date": "2022-02-03T19:06:55.717Z",
       "__v": 0
-    }
+    },
+    {
+        "_id": "62041fa143d2a1fc96ae19c8",
+        "user": "61fbf7493e28afe06125c073",
+        "title": "My new note",
+        "description": "Wake up its early morning",
+        "tag": "personal",
+        "date": "2022-02-09T20:10:09.304Z",
+        "__v": 0
+      },{
+        "_id": "61fc27cfdb201474814ebe48",
+        "user": "61fbf7493e28afe06125c073",
+        "title": "My Title",
+        "description": "Wake up its morning",
+        "tag": "personal",
+        "date": "2022-02-03T19:06:55.717Z",
+        "__v": 0
+      },
+      {
+          "_id": "62041fa143d2a1fc96ae19c8",
+          "user": "61fbf7493e28afe06125c073",
+          "title": "My new note",
+          "description": "Wake up its early morning",
+          "tag": "personal",
+          "date": "2022-02-09T20:10:09.304Z",
+          "__v": 0
+        },{
+            "_id": "61fc27cfdb201474814ebe48",
+            "user": "61fbf7493e28afe06125c073",
+            "title": "My Title",
+            "description": "Wake up its morning",
+            "tag": "personal",
+            "date": "2022-02-03T19:06:55.717Z",
+            "__v": 0
+          },
+          {
+              "_id": "62041fa143d2a1fc96ae19c8",
+              "user": "61fbf7493e28afe06125c073",
+              "title": "My new note",
+              "description": "Wake up its early morning",
+              "tag": "personal",
+              "date": "2022-02-09T20:10:09.304Z",
+              "__v": 0
+            }
   ]
   const[notes,setNotes]=useState(initialnotes)
     return(
