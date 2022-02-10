a2454a5ea9d33aee3ebd63ab6f0810576d5bed2b
Darshit Sorathiya <darshitgsorathiya7@gmail.com>
2022-02-11 00:49:41 +0530

    till delete note

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,17 +1,21 @@
 import {React,useContext} from 'react'
 import noteContext from '../context/notes/noteContext';
 import Noteitem from './Noteitem';
+import Addnote from './Addnote';
 
 const Notes = () => {
     const context=useContext(noteContext);
-  const {notes,setNotes}=context;
+  const {notes}=context;
   return (
+      <>
+      <Addnote/>
     <div className='row my-3'>
          <h2>Your Notes</h2>
       {notes.map((note)=>{
-          return <Noteitem note={note}/>
+          return <Noteitem key={note._id} note={note}/>
         })}
     </div>
+    </>
   )
 }
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -5,7 +5,7 @@ const NoteState=(props)=>{
 
 const initialnotes=[
     {
-      "_id": "61fc27cfdb201474814ebe48",
+      "_id": "61fc27cfdb201474814ebe481",
       "user": "61fbf7493e28afe06125c073",
       "title": "My Title",
       "description": "Wake up its morning",
@@ -14,7 +14,7 @@ const initialnotes=[
       "__v": 0
     },
     {
-        "_id": "62041fa143d2a1fc96ae19c8",
+        "_id": "62041fa143d2a1fc96ae19c82",
         "user": "61fbf7493e28afe06125c073",
         "title": "My new note",
         "description": "Wake up its early morning",
@@ -22,7 +22,7 @@ const initialnotes=[
         "date": "2022-02-09T20:10:09.304Z",
         "__v": 0
       },{
-        "_id": "61fc27cfdb201474814ebe48",
+        "_id": "61fc27cfdb201474814ebe483",
         "user": "61fbf7493e28afe06125c073",
         "title": "My Title",
         "description": "Wake up its morning",
@@ -31,7 +31,7 @@ const initialnotes=[
         "__v": 0
       },
       {
-          "_id": "62041fa143d2a1fc96ae19c8",
+          "_id": "62041fa143d2a1fc96ae19c84",
           "user": "61fbf7493e28afe06125c073",
           "title": "My new note",
           "description": "Wake up its early morning",
@@ -39,7 +39,7 @@ const initialnotes=[
           "date": "2022-02-09T20:10:09.304Z",
           "__v": 0
         },{
-            "_id": "61fc27cfdb201474814ebe48",
+            "_id": "61fc27cfdb201474814ebe485",
             "user": "61fbf7493e28afe06125c073",
             "title": "My Title",
             "description": "Wake up its morning",
@@ -48,7 +48,7 @@ const initialnotes=[
             "__v": 0
           },
           {
-              "_id": "62041fa143d2a1fc96ae19c8",
+              "_id": "62041fa143d2a1fc96ae19c86",
               "user": "61fbf7493e28afe06125c073",
               "title": "My new note",
               "description": "Wake up its early morning",
@@ -58,8 +58,33 @@ const initialnotes=[
             }
   ]
   const[notes,setNotes]=useState(initialnotes)
+
+//* Add a Note
+const addNote=(title,description,tag)=>{
+   const note={
+    "_id": "62041fa143d2a1143fc96ae19c86",
+    "user": "61fbf7493e28afe06125c073",
+    "title": title,
+    "description": description,
+    "tag": tag,
+    "date": "2022-02-09T20:10:09.304Z",
+    "__v": 0
+  }
+    setNotes(notes.concat(note));
+}
+
+//* Delete a Note
+const deleteNote=(id)=>{
+    console.log("Deleting the note with id : "+id);
+    const newNotes=notes.filter((note)=>{return note._id!==id});
+    setNotes(newNotes);
+}
+
+//* Edit a Note
+const editNote=(id)=>{}
+
     return(
-        <NoteContext.Provider value={{notes,setNotes}}>
+        <NoteContext.Provider value={{notes,addNote,deleteNote,editNote}}>
             {props.children}
         </NoteContext.Provider>
     )
